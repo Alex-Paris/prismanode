05cb8b880ded2accb0d485100c8f58f44d2e417e
Alex Paris <alexlima@gruposgapetro.com.br>
2022-03-19 07:20:47 -0300

    chore: upsert user with group

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -11,13 +11,45 @@ export default class UserController {
   }
 
   public async store(request: Request, response: Response): Promise<Response> {
-    const { id, name, email } = request.body;
+    const { id, name, email, groups } = request.body;
 
-    const newUser = { id, name, email };
-
-    const user = await prisma.user.create({
-      data: newUser
+    const user = await prisma.user.upsert({
+      where: {
+        id
+      },
+      update: {
+        name,
+        email,
+        groups: {
+          upsert: {
+            where: {
+              id: groups.id
+            },
+            update: {
+              title: groups.title
+            },
+            create: groups
+          }
+        }
+      },
+      create: {
+        id,
+        name,
+        email,
+        groups: {
+          connectOrCreate: {
+            create: groups,
+            where: {
+              id: groups.id
+            }
+          }
+        }
+      }
     })
+
+    // const user = await prisma.user.create({
+    //   data: newUser
+    // })
 
     return response.json(user);
   }
